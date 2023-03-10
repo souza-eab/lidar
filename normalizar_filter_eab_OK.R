3205cbc772c81c80de4efb0565a0a0a8dc11c4d3
Edriano Souza <79071899+souza-eab@users.noreply.github.com>
2023-03-10 10:41:44 -0300

    Update normalizar_filter_eab_OK.R

--- normalizar_filter_eab_OK.R
+++ normalizar_filter_eab_OK.R
@@ -1,3 +1,6 @@
+Sys.setenv(R_NUM_THREADS = "8") # Set your (Cores - 1) (Allocate your Web)
+
+
 #### Clipar e normalizar nuvens - Tanguro
 
 library(lidR)
@@ -12,13 +15,14 @@ setwd("D:/Trabalhando/Nuvens_IFG_UFG/XYZ/las")
 files = dir(pattern = '\\.las$')
 outDir.norm<-paste("D:/Trabalhando/Nuvens_IFG_UFG/XYZ/las/Normalized")
 outDir.clip<-paste("D:/Trabalhando/Nuvens_IFG_UFG/XYZ/las/Clipped")
+outDir.chm <- paste("D:/Trabalhando/Nuvens_IFG_UFG/XYZ/las/chm_norm")
 
 #Visualize cloud
 cld<-lidR::readLAS(files[1])
 plot(cld) ; axes3d(col='white')
 
-lasground
-grid_terrain
+#lasground
+#grid_terrain
 
 for(i in 1:length(files)){
   file = files[i]
@@ -42,11 +46,27 @@ for(i in 1:length(files)){
   #Clipar para a subparcela de 10x10m
   cld.clip<-clip_rectangle(cld.norm, -5, -5, 5, 5)
   
+  # ch
+  chm <-  cld.clip
+  #chm@header@crs$projargs <- "+init=epsg:4326"
+  #chm.grid <- grid_metrics(cld.norm, max(Z), 1, filter = NULL)
+  b <- extent(chm) # BoundBox
+  r <- raster(b, res = 1) #Your resolution
+  m <- grid_metrics(chm, ~mean(Z), r) # Eg. CHM 
+  
+  plot(m)
+  plot(extent(m), add = T, col = "red", lwd = 2)
+  
+  colors <- height.colors(50)
+  plot(chm, col=colors)
+  
   # name files to be written
   outLas.norm = paste(outDir.norm, sub('.laz','_norm.laz',file,fixed = T), sep='/')
   outLas.clip = paste(outDir.clip, sub('.laz','_clip.laz',file,fixed = T), sep='/')
+  outCHM.norm = paste(outDir.chm, sub('.tif','_chm.tif', file, fixed = T), sep = '/')
   
   # write normalized files
   writeLAS(cld.norm, outLas.norm) 
   writeLAS(cld.clip, outLas.clip)
+  writeRaster(m, outCHM.norm, format="GTiff", overwrite=TRUE)
 }
